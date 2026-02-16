44f00085971150c1bea293686f4c56a73b5633a1
Ayaan Thazhathethil <aa1778@srmist.edu.in>
2026-02-16 14:04:16 +0530

    UC3: Rewrite banner printing using String.join for better performance

---
+++ OOPSBannerApp/OOPSUC3.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class OOPSUC3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class OOPSUC3
+{
+    
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ", " *** ", " *** ", "**** ", " ****"));
+        System.out.println(String.join("   ", "*   *", "*   *", "*   *", "*    "));
+        System.out.println(String.join("   ", "*   *", "*   *", "**** ", " *** "));
+        System.out.println(String.join("   ", "*   *", "*   *", "*     ", "    *"));
+        System.out.println(String.join("   ", " *** ", " *** ", "*     ", "**** "));
+
+    }
+
+
+}
