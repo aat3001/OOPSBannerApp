ffa213820c59967974ce651384525051364a085c
Ayaan Thazhathethil <aa1778@srmist.edu.in>
2026-02-16 13:34:36 +0530

    UC2: Add banner display using print statements

---
+++ OOPSBannerApp/OOPSBannerAppUC2.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class OOPSBannerAppUC2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class OOPSBannerAppUC2
+{
+     public static void main(String[] args) {    
+    
+ 
+ 
+
+
+        // Each letter is 7 lines tall and properly aligned
+
+    System.out.println("  *****   " + "  *****   " + "  ******  " + "  *****   ");
+    System.out.println(" **   **  " + " **   **  " + " **    ** " + " **   **  ");
+    System.out.println("**     ** " + "**     ** " + " **    ** " + "**        ");
+    System.out.println("**     ** " + "**     ** " + " ******   " + "  *****   ");
+    System.out.println("**     ** " + "**     ** " + " **        " + "       ** ");
+    System.out.println(" **   **  " + " **   **  " + " **        " + " **    ** ");
+    System.out.println("  *****   " + "  *****   " + " **        " + "  *****   ");
+}
+}
