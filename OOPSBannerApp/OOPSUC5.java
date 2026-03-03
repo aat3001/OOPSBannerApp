09c3b007a646da849da1ef7bd62c641c6f395a01
Ayaan Thazhathethil <aa1778@srmist.edu.in>
2026-03-03 14:54:37 +0530

    UC8 - Implement HashMap based character pattern system for OOPS banner

--- OOPSBannerApp/OOPSUC5.java
+++ OOPSBannerApp/OOPSUC5.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class OOPSUC5 here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class OOPSUC5
 {
 
---
+++ OOPSBannerApp/OOPSUC8.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class OOPSUC8{
+
+    // Method to create HashMap with character patterns
+    public static HashMap<Character, String[]> createCharacterMap() {
+
+        HashMap<Character, String[]> charMap = new HashMap<>();
+
+        // Pattern for O
+        charMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        // Pattern for P
+        charMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        // Pattern for S
+        charMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+
+        return charMap;
+    }
+
+    // Method to display banner
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+
+        int patternHeight = charMap.get('O').length;
+
+        for (int line = 0; line < patternHeight; line++) {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+
+                String[] pattern = charMap.get(ch);
+                sb.append(pattern[line]).append(" ");
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        HashMap<Character, String[]> charMap = createCharacterMap();
+
+        String message = "OOPS";
+
+        displayBanner(message, charMap);
+    }
+}
